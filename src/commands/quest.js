7c50551d31a09ab14050dd9489c110fe495ec126
lozaine89 <laurieginag@gmail.com>
2025-09-01 01:31:29 +0800

    Fix import path for Player model in quest.js

--- src/commands/quest.js
+++ src/commands/quest.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const questManager = require('../quests/QuestManager');
-const { Player } = require('../database'); // Adjust the path according to your project structure
+const { Player } = require('../database/models/player'); 
 
 module.exports = {
     data: new SlashCommandBuilder()
